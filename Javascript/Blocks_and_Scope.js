28a78c30a7b4ce858f9efd26170e46f4ff3a0b51
TheoBreakspear <theo.breakspear@gmail.com>
2021-07-15 13:23:25 +0100

    Added example of block scope

--- Javascript/Blocks_and_Scope.js
+++ Javascript/Blocks_and_Scope.js
@@ -4,5 +4,13 @@ function logCitySkyline(){
   let skyscraper = 'Empire State Building';
   return 'The stars over the ' + skyscraper + ' in ' + city;
 }
-
+//Global Scope
 console.log(logCitySkyline());
+
+function logVisibleLightWaves(){
+   const lightWaves = 'Moonlight';
+   console.log(lightWaves);
+ }
+ //Block Scope
+ logVisibleLightWaves();
+ console.log(lightWaves);
