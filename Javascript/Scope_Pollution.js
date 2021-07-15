05983058b6434456a1837ea871acfe6160f73b81
TheoBreakspear <theo.breakspear@gmail.com>
2021-07-15 13:41:27 +0100

    scope pollution example

---
+++ Javascript/Scope_Pollution.js
@@ -0,0 +1,15 @@
+/*stars is globally defined, but
+when it is reassigned in a function
+its value changes globally
+*/
+const satellite = 'The Moon';
+const galaxy = 'The Milky Way';
+let stars = 'North Star';
+
+const callMyNightSky = () => {
+  stars = 'Sirius';
+	return 'Night Sky: ' + satellite + ', ' + stars + ', ' + galaxy;
+};
+
+console.log(callMyNightSky());
+console.log(stars)
