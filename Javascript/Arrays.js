0030c21a2f001758d8d35d79b448349dd3ede744
TheoBreakspear <theo.breakspear@gmail.com>
2021-07-16 13:35:07 +0100

    completed excercise

--- Javascript/Arrays.js
+++ Javascript/Arrays.js
@@ -1,3 +1,16 @@
-let condiments = ['Ketchup', 'Mustard', 'Soy Sauce', 'Sriracha'];
+llet condiments = ['Ketchup', 'Mustard', 'Soy Sauce', 'Sriracha'];
 
 const utensils = ['Fork', 'Knife', 'Chopsticks', 'Spork'];
+
+condiments[0] = 'Mayo';
+console.log(condiments);
+
+condiments = ['Mayo'];
+console.log(condiments);
+
+utensils[3] = 'Spoon';
+console.log(utensils);
+
+//utensils = ['stuff'];
+//console.log(utensils);
+//^const arrays are mutable, but not reassignable
