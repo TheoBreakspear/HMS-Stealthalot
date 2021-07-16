305f318d8d4ba28520a00317ac7ba513f7c08f4b
TheoBreakspear <theo.breakspear@gmail.com>
2021-07-16 18:36:26 +0100

    push method

--- Javascript/Arrays.js
+++ Javascript/Arrays.js
@@ -2,5 +2,5 @@ let condiments = ['Ketchup', 'Mustard', 'Soy Sauce', 'Sriracha'];
 
 const utensils = ['Fork', 'Knife', 'Chopsticks', 'Spork'];
 
-utensils.push('other', 'things');
+utensils.push('other', 'things'); //equivalent to append in python
 console.log(utensils);
