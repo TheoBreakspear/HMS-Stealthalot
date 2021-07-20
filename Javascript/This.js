cbd9acebcc9720b077b82cb0d83bd1db6627c6f6
TheoBreakspear <theo.breakspear@gmail.com>
2021-07-20 14:46:21 +0100

    created this ex

---
+++ Javascript/This.js
@@ -0,0 +1,9 @@
+//do not use arrow functions with this because it will refer to the global object
+const robot = {
+   energyLevel: 100,
+   checkEnergy: () => {
+     console.log(`Energy is currently at ${this.energyLevel}%.`)
+   }
+ }
+ 
+ robot.checkEnergy();
---
+++ Javascript/test.js
@@ -0,0 +1,13 @@
+testObj = {
+   a: 'A',
+   b: 'B',
+   c: {
+      first: 1,
+      second: 2,
+      third: 3
+   }
+}
+
+for (let i in testObj){
+   console.log(testObj[i])
+};
