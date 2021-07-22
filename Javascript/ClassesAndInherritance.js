6e07ab87b5d27cf3e0a551665c48c264c503caea
TheoBreakspear <theo.breakspear@gmail.com>
2021-07-22 18:07:09 +0100

    created classes and inherritance ex

---
+++ Javascript/ClassesAndInherritance.js
@@ -0,0 +1,22 @@
+//superclass
+class HospitalEmployee {
+   constructor(name){
+     this._name = name;
+     this._remainingVacationDays = 20;
+   }
+ 
+   get name() {
+     return this._name;
+   }
+ 
+   get remainingVacationDays() {
+     return this._remainingVacationDays;
+   }
+ 
+   takeVacationDays(daysOff) {
+     this._remainingVacationDays -= daysOff;
+   }
+ 
+ }
+
+ 
